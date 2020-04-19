6c54f4e88c3ad0a6a17defbbb825fd865ad4a267
akshaya999 <39755555+akshaya999@users.noreply.github.com>
2020-04-19 02:18:30 -0700

    invalid path prevention

--- jsonv1.py
+++ jsonv1.py
@@ -3,31 +3,30 @@ import sys
 import json
 
 def check_path(path):
-   if os.path.exists(path) and os.path.isdir(path):
-      return True
+    return os.path.exists(path) and os.path.isdir(path)
 
 def jasondata(source,destination):
     datalist = dict()
     datalist["Label_Format"] = ""
     datalist["training"] = []
     path = sys.argv[1]
-    if (check_path(source)):
-        for x in os.listdir(source):
-            if x.endswith("png"):
-                temp = dict()
-                temp["image"] = x
-                temp["label"] = ""
-                datalist["training"].append(temp)
+    try:
+        if (check_path(source)):
+            for x in os.listdir(source):
+                if x.endswith("png"):
+                    temp = dict()
+                    temp["image"] = x
+                    temp["label"] = ""
+                    datalist["training"].append(temp)
 
-    json_object = json.dumps(datalist, indent=4)
-    final_destination= destination + "/datalist.json"
-    with open(final_destination, "w") as outfile:
-        outfile.write(json_object)
+            json_object = json.dumps(datalist, indent=4)
+            final_destination= destination + "/datalist.json"
+            with open(final_destination, "w") as outfile:
+                outfile.write(json_object)
+    except:
+        print("Please enter a valid path!")
       	
 if __name__=="__main__":
     jasondata(sys.argv[1],sys.argv[2])
-
-       
-
 
        
