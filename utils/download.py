64d2608e7ef1d6a6b89dc6e75a6e86cb5d6454c8
arroyoan <anoel1214@gmail.com>
2020-05-18 10:31:03 -0700

    added some more defaults

--- utils/download.py
+++ utils/download.py
@@ -2,46 +2,55 @@ from dicomweb_client.api import DICOMwebClient
 import json
 import sys
 
-def get_info(server_name):
+def get_info(server_name,studyIds):
     client = DICOMwebClient(url=server_name,
             qido_url_prefix='rs',
             wado_url_prefix='rs',
             stow_url_prefix='rs')
 
-    a = client.search_for_series('1.2.826.0.1.3680043.8.1055.1.20111102150758591.92402465.76095170')
-
+    #a = client.search_for_series('1.2.826.0.1.3680043.8.1055.1.20111102150758591.92402465.76095170')
+    # print(a[0]['0020000D'])
     # with open('out.json', 'w') as f:
     #     f.write(json.dumps(studies))
 
 
-    #studies_subset = client.search_for_studies(limit=5)
-    # for s in studies_subset:
-    #     for k, v in s.items():
-    #         if v['vr'] == 'UI':
-    #             print(v)
+    studies_subset = client.search_for_studies(limit=5)
+    singleStudy = client.search_for_studies(search_filters={'0020000D':'1.3.6.1.4.1.25403.345050719074.3824.20170125112931.11'})
+    print(singleStudy[0]['0020000D']['Value'][0])
+    #print(studies_subset[2]['0020000D']['Value'][0])
+    print("after single study")
+    for index in range(len(studyIds)):
+        singleStudy = client.search_for_studies(search_filters={'0020000D':studyIds[index]})
+        print(index,len(singleStudy))
+    # for index in range(len(studies_subset)):
+    #     print(studies_subset[index]['0020000D']['Value'][0])
+        
+        # for k, v in s.items():
+        #     if v['vr'] == 'UI':
+        #         print(v)
 
-    study_instance_uid = a[0]['0020000D']['Value'][0]
-    series_instance_uid = a[0]['0020000E']['Value'][0]
-    print(study_instance_uid)
-    print(series_instance_uid)
+    #study_instance_uid = a[0]['0020000D']['Value'][0]
+    #series_instance_uid = a[0]['0020000E']['Value'][0]
+    #print(study_instance_uid)
+    #print(series_instance_uid)
 
     # instances = client.retrieve_series(
     #     study_instance_uid=study_instance_uid,
     #     series_instance_uid=series_instance_uid
     # )
 
-    instances = client.search_for_instances(
-        study_instance_uid=study_instance_uid,
-        series_instance_uid=series_instance_uid
-    )
-    sop_instance_uid = instances[0]['00080018']['Value'][0]
+    # instances = client.search_for_instances(
+    #     study_instance_uid=study_instance_uid,
+    #     series_instance_uid=series_instance_uid
+    # )
+    # sop_instance_uid = instances[0]['00080018']['Value'][0]
 
-    instance = client.retrieve_instance(
-        study_instance_uid=study_instance_uid,
-        series_instance_uid=series_instance_uid,
-        sop_instance_uid = sop_instance_uid,
-       )
-    print(instance)
+    # instance = client.retrieve_instance(
+    #     study_instance_uid=study_instance_uid,
+    #     series_instance_uid=series_instance_uid,
+    #     sop_instance_uid = sop_instance_uid,
+    #    )
+    # print(instance)
 
     # study_instance_uid = a[0]['0020000D']['Value'][0]
     # series_instance_uid = a[0]['0020000E']['Value'][0]
@@ -64,6 +73,14 @@ def main(argv):
     #this checks that there are enough args, will add something that that returns if not enough
     if(len(argv)<2):
         print("This is not enough args")
+        #this is some test data
+        studyIds =[
+            '1.3.6.1.4.1.25403.345050719074.3824.20170126083429.2',
+            '1.3.6.1.4.1.25403.345050719074.3824.20170125112931.11',
+            '1.2.840.113619.2.30.1.1762295590.1623.978668949.886'
+            '1.2.840.113619.2.67.2158294438.15745010109084247.20000',
+            '1.3.6.1.4.1.25403.345050719074.3824.20170125095258.1'
+        ]
     
     #This line adds the list of studyIds as variable to the program
     else:
@@ -75,7 +92,7 @@ def main(argv):
     else:
         url = 'http://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE'
 
-    get_info(url)
+    get_info(url,studyIds)
 
 if __name__ == '__main__':
     main(sys.argv)
