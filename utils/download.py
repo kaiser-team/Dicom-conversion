a466bf095f74c86cd959ff96a429173bd56310d7
shanshs <50124262+shanshs@users.noreply.github.com>
2020-05-18 23:42:10 -0400

    Update download.py

--- utils/download.py
+++ utils/download.py
@@ -1,63 +1,80 @@
 from dicomweb_client.api import DICOMwebClient
-import json
 
-def get_info(server_name):
-    client = DICOMwebClient(url=server_name,
-                            qido_url_prefix='rs',
-                            wado_url_prefix='rs',
-                            stow_url_prefix='rs')
+class Meta:
+    def __init__(self, url):
+        self.url = url
+        self.client = None
 
-    a = client.search_for_series('1.2.826.0.1.3680043.8.1055.1.20111102150758591.92402465.76095170')
+    def create_client(self):
+        client = DICOMwebClient(url=self.url,
+                                qido_url_prefix='rs',
+                                wado_url_prefix='rs',
+                                stow_url_prefix='rs')
+        self.client = client
 
-    # with open('out.json', 'w') as f:
-    #     f.write(json.dumps(studies))
+    def get_series_UID(self, study_uid):
+        studies = self.client.search_for_series(search_filters={'0020000D': study_uid})
+        all_series = []
+        for study in studies:
+            all_series.append(study['0020000E']['Value'][0])
 
+        print(all_series)
+        return all_series
 
-    #studies_subset = client.search_for_studies(limit=5)
-    # for s in studies_subset:
-    #     for k, v in s.items():
-    #         if v['vr'] == 'UI':
-    #             print(v)
+    def get_sop_UID(self, study_uid, series):
+        sop = []
+        for serie in series:
+            instances = self.client.search_for_instances(
+                study_instance_uid=study_uid,
+                series_instance_uid=serie)
+            print(instances)
+            sop.append(instances[0]['00080018']['Value'][0])
+        return sop
 
-    study_instance_uid = a[0]['0020000D']['Value'][0]
-    series_instance_uid = a[0]['0020000E']['Value'][0]
-    print(study_instance_uid)
-    print(series_instance_uid)
+    def retrieve_instance(self, study_uid):
+        instances = self.client.retrieve_study(study_uid)
+        print(type(instances[0]))
 
-    # instances = client.retrieve_series(
-    #     study_instance_uid=study_instance_uid,
-    #     series_instance_uid=series_instance_uid
-    # )
+    def retrieve(self, study_uid, series, sop):
+        instance = self.client.retrieve_instance(
+            study_instance_uid=study_uid,
+            series_instance_uid='1.2.826.0.1.3680043.2.1125.1.75064541463040.2005072610384286453',
+            sop_instance_uid='1.2.826.0.1.3680043.2.1125.1.75064541463040.2005072610384286470',
+            media_types=(('image/jp2', '1.2.840.10008.1.2.4.90',),))
 
-    instances = client.search_for_instances(
-        study_instance_uid=study_instance_uid,
-        series_instance_uid=series_instance_uid
-    )
-    sop_instance_uid = instances[0]['00080018']['Value'][0]
+        print(instance)
+        with open("1.jpg", "wb") as fh:
+            fh.write(instance)
 
-    instance = client.retrieve_instance(
-        study_instance_uid=study_instance_uid,
-        series_instance_uid=series_instance_uid,
-        sop_instance_uid = sop_instance_uid,
-       )
-    print(instance)
-
-    # study_instance_uid = a[0]['0020000D']['Value'][0]
-    # series_instance_uid = a[0]['0020000E']['Value'][0]
-    # sop_class_uid = a[0]['00080016']['Value'][0]
-    # sop_instance_uid = a[0]['00080018']['Value'][0]
-
-
-    # frames = client.retrieve_instance_frames(
-    #     study_instance_uid=study_instance_uid,
-    #     series_instance_uid = series_instance_uid,
-    #     sop_instance_uid = sop_instance_uid,
-    #     frame_numbers = [1],
-    #     media_types = ('image/jpeg', ))
+        # for index, serie in enumerate(series):
+        #     instance = self.client.retrieve_instance(
+        #         study_instance_uid=study_uid,
+        #         series_instance_uid=serie,
+        #         sop_instance_uid=sop[index],
+        #         media_types=(('image/jp2', '1.2.840.10008.1.2.4.90',),)
+        #     )
+        #     print(instance)
+        #     with open(str(index)+".jpg", "wb") as fh:
+        #         fh.write(instance)
 
 
 if __name__ == '__main__':
+    with open('1.2.826.0.1.3680043.2.1125.1.75064541463040-2.2005072610384286470',
+              mode='rb') as file:
+        fileContent = file.read()
 
-    #url = "https://dicomcloud.azurewebsites.net"
-    url = 'http://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE'
-    get_info(url)
+    print(fileContent)
+
+    with open("2.dcm", "wb") as fh:
+        fh.write(fileContent)
+
+
+    # url = 'http://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE'
+    # #url = 'http://localhost:8080/dcm4chee-arc/aets/DCM4CHEE'
+    # meta = Meta(url)
+    # meta.create_client()
+    # study_uid = '1.2.826.0.1.3680043.8.1055.1.20111102150758591.92402465.76095170'
+    # study_uid = '1.2.826.0.1.3680043.2.1125.1.75064541463040.2005072610384286421'
+    # series = meta.get_series_UID(study_uid)
+    # sop = meta.get_sop_UID(study_uid, series)
+    # meta.retrieve(study_uid, series, sop)
