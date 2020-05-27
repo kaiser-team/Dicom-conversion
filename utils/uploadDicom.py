bd0cc22ec7ca798665853dcfc06270cc7a1f333c
Shanshan Shi <shanshs@uci.edu>
2020-05-27 15:10:07 -0400

    uploading dicoms

--- utils/uploadDicom.py
+++ utils/uploadDicom.py
@@ -2,10 +2,6 @@ import os
 import sys
 
 
-def store_to_server(src, server_url):
-    os.system('cmd /c "storescu +sd +sp *.dcm -aec DCM4CHEE localhost 11112 "' + '\"' + src + '\"')
-
-
 if __name__ == '__main__':
     try:
         server = sys.argv[1]    # destination folder
